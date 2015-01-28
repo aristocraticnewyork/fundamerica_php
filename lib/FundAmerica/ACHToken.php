8dae327da79f2c3bbc117a7bd54e2c88779d4aaa
Dave Nguyen <Dave.Nguyen@inthenight.net>
2015-01-28 15:51:02 -0800

    Exceptions for all/find

--- lib/FundAmerica/ACHToken.php
+++ lib/FundAmerica/ACHToken.php
@@ -3,9 +3,17 @@
 class ACHToken extends APIResource {
   protected static $objectName = 'ach_tokens';
 
+  public function all() {
+    throw new InvalidAllException();
+  }
+
   public function create($entityId) {
     $postFields = 'entity_id=' . $entityId;
     return self::post($postFields);
+  }
+
+  public function find() {
+    throw new InvalidFindException();
   }
 }
 
