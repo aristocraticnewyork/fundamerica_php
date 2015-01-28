d967df226f1c314f8f7b74b9670a7e48962c55e6
Dave Nguyen <Dave.Nguyen@inthenight.net>
2015-01-28 15:15:34 -0800

    get/get_all/post/patch-able

--- lib/FundAmerica/APIResource.php
+++ lib/FundAmerica/APIResource.php
@@ -2,25 +2,71 @@
 
 abstract class APIResource
 {
-  public function call() {
+  private function call($curlopts = null) {
     if (!isset(static::$objectName)) {
       throw new Exception('No object name provided.');
     }
 
-    $ch = curl_init(FundAmerica::apiURL() . static::$objectName);
+    $ch = curl_init();
+    $url = FundAmerica::apiURL() . static::$objectName;
     $user = FundAmerica::apiKey() . ':';
 
+    if (isset($curlopts['id'])) {
+      $url = $url . '/' . $curlopts['id'];
+      unset($curlopts['id']);
+    }
+
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_USERPWD, $user);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, static::$postFields);
+    if (isset($curlopts)) {
+      foreach ($curlopts as $key => $value) {
+        curl_setopt($ch, $key, $value);
+      }
+    }
 
     $response = curl_exec($ch);
     curl_close($ch);
     return json_decode($response, true);
   }
 
-  abstract public function create($opts);
+  public function get($id) {
+    $curlopts = array(
+      'id' => $id
+    );
+    return self::call($curlopts);
+  }
+
+  public function get_all() {
+    return self::call();
+  }
+
+  public function post($postFields) {
+    if (is_array($postFields)) {
+      $postFields = join('&', $postFields);
+    }
+
+    $curlopts = array(
+      CURLOPT_POST => 1,
+      CURLOPT_POSTFIELDS => $postFields
+    );
+
+    return self::call($curlopts);
+  }
+
+  public function patch($id, $postFields) {
+    if (is_array($postFields)) {
+      $postFields = join('&', $postFields);
+    }
+
+    $curlopts = array(
+      'id' => $id,
+      CURLOPT_CUSTOMREQUEST => 'PATCH',
+      CURLOPT_POSTFIELDS => $postFields
+    );
+
+    return self::call($curlopts);
+  }
 }
 
 ?>
